76deda4e8bf86c6b0e87f9cfe2fe6e909d560e49
Wiley Conte <wileymckayconte@gmail.com>
2024-02-01 16:05:20 -0700

    Add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/png"
+          sizes="32x32"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
